bba57181716f2084789324e723ee268084c896ba
Dorion de Gobeo <ddegobeo@Thunkpad.dingle>
2021-07-12 20:12:22 +0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Hoek = require('@hapi/hoek');
+const Mongoose = require('mongoose');
+const tesseract = require("node-tesseract-ocr")
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 8000,
+        host: '0.0.0.0'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        options: { 
+            cors: {
+                origin: ['*']
+            }
+        },
+        handler: (request, h) => {
+           
+            return 'hellllllooo!';
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/doOCR',
+        options: { 
+            cors: {
+                origin: ['*']
+            }
+        },
+        handler: (request, h) => {
+           
+            const image = request.payload.image;
+            const config = {
+                lang: 'deu',
+                oem: 1,
+                psm: 11,
+                tessedit_char_whitelist: 'abcdefghijklmnopqrstuvwxyzöäü'
+            }
+            
+            if(!image) {
+                return h.response();
+            }
+            
+            const base64string = image.split(',')[1];
+            
+            return tesseract
+                .recognize(Buffer.from(base64string, 'base64'), config)
+                .then(async (text) => {
+                    console.log("Result:", text)
+                    
+                    const rankedProducts = await Product.find({$text: {$search: text}}, {score: {$meta: 'textScore'}}).sort({score: {$meta: 'textScore'}})
+                    console.log(rankedProducts);
+                    // const product = new Product({ name: 'Olive Oil', text });
+                    // product.save();
+                    
+                    return { text: Hoek.reach(rankedProducts, [0, 'name']) || 'No Matching Products' };
+                })
+                .catch((e) => {
+                    
+                    console.log(e);
+                    return e;
+                });
+        }
+    });
+
+    Mongoose.connect('mongodb://localhost:27017/darkly', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    const Product = Mongoose.model('product', new Mongoose.Schema({ 
+        name: String,
+        text: {
+            type: String,
+            index: true
+        }
+    }));
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
